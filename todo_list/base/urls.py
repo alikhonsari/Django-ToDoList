919fa5ec26e508eb910eabe27546f4702d532981
alikhonsari <63183897+alikhonsari@users.noreply.github.com>
2022-08-20 16:35:11 +0430

    Add files via upload

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -1,8 +1,16 @@
 import imp
 from django.urls import path
-from .views import TaskList, TaskDetail
+from .views import TaskCreate, TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView, CustomLoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+
+    path('login/', CustomLoginView.as_view(), name = 'login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name = 'logout'),
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('create-task/', TaskCreate.as_view(), name ='task-create'),
+    path('task-edit/<int:pk>/', TaskUpdate.as_view(), name ='task-update'),
+    path('task-delete/<int:pk>/', DeleteView.as_view(), name ='task-delete'),
+
 ]
